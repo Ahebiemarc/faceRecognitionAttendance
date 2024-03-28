5d9613fe769df92243e8a9bb8f92fafa10eab87f
Ahbiemarc <ahebiemarc22@gmail.com>
2024-03-28 13:14:28 +0100

    first commit

---
+++ util.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+def get_button(window, text, color, command, fg='white'):
+    button = tk.Button(
+        window,
+        text=text,
+        activebackground='black',
+        activeforeground='white',
+        fg=fg,
+        bg=color,
+        command=command,
+        height=2,
+        width=28,
+        font=('Helvetica Bold', 20)
+    )
+    return button
+
+
+def get_img_label(window):
+    label = tk.Label(window)
+    label.grid(row=0, column=0)
+    return label
+
+
+def get_entry_text(window):
+    inputText = tk.Text(window, height=2, width=15, font=('Arial', 32))
+    return inputText
+
+
+def msg_box(title, desc):
+    messagebox.showinfo(title, desc)
